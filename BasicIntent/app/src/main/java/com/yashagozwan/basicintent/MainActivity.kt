7a6c4b45bdb97e0f721e8fe0a4be3ef7ed800713
Yasha Gozwan Shuhada <yashagozwan13@gmail.com>
2024-12-01 00:23:00 +0700

    move activity for result

--- BasicIntent/app/src/main/java/com/yashagozwan/basicintent/MainActivity.kt
+++ BasicIntent/app/src/main/java/com/yashagozwan/basicintent/MainActivity.kt
@@ -1,17 +1,18 @@
 package com.yashagozwan.basicintent
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var tvResult: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -21,6 +22,12 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         findViewById<Button>(R.id.btn_move_activity_data).setOnClickListener(this)
 
         findViewById<Button>(R.id.btn_move_activity_object).setOnClickListener(this)
+
+        findViewById<Button>(R.id.btn_dial_number).setOnClickListener(this)
+
+        findViewById<Button>(R.id.btn_move_for_result).setOnClickListener(this)
+
+        tvResult = findViewById(R.id.tv_result)
     }
 
     private fun moveActivity(view: View) {
@@ -49,11 +56,38 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     }
 
+    private fun doDialPhone() {
+        val phone = "0813618327373"
+        Intent(Intent.ACTION_DIAL, Uri.parse("tel:$phone"))
+            .also(::startActivity)
+    }
+
+
+    private val resultLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+        if (it.resultCode == MoveForResultActivity.RESULT_CODE && it.data != null) {
+            val selectedValue =
+                it.data?.getIntExtra(MoveForResultActivity.EXTRA_SELECTED_VALUE, 0)
+
+            tvResult.text = "Hasil: $selectedValue"
+        }
+    }
+
+
+    private fun doChooseNumber() {
+        Intent(this, MoveForResultActivity::class.java).also {
+            resultLauncher.launch(it)
+        }
+    }
+
     override fun onClick(view: View) {
         when (view.id) {
             R.id.btn_move_activity -> moveActivity(view = view)
             R.id.btn_move_activity_data -> moveActivityData(view = view)
             R.id.btn_move_activity_object -> moveActivityWithObject()
+            R.id.btn_dial_number -> doDialPhone()
+            R.id.btn_move_for_result -> doChooseNumber()
         }
     }
 }
---
+++ BasicIntent/app/src/main/java/com/yashagozwan/basicintent/MoveForResultActivity.kt
@@ -0,0 +1,63 @@
+package com.yashagozwan.basicintent
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.view.View.OnClickListener
+import android.widget.Button
+import android.widget.RadioGroup
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MoveForResultActivity : AppCompatActivity(), OnClickListener {
+
+    private lateinit var btnChoose: Button
+    private lateinit var rgNumber: RadioGroup
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_move_for_result)
+
+        btnChoose = findViewById(R.id.btn_choose)
+        rgNumber = findViewById(R.id.rg_number)
+
+        btnChoose.setOnClickListener(this)
+
+    }
+
+
+    private fun doSelectNumber() {
+
+        if (rgNumber.checkedRadioButtonId > 0) {
+            var value = 0
+            when (rgNumber.checkedRadioButtonId) {
+                R.id.rb_50 -> value = 50
+                R.id.rb_100 -> value = 100
+                R.id.rb_150 -> value = 150
+                R.id.rb_200 -> value = 200
+            }
+            
+            Intent().apply {
+                putExtra(EXTRA_SELECTED_VALUE, value)
+            }.also {
+                setResult(RESULT_CODE, it)
+                finish()
+            }
+
+        }
+    }
+
+
+    override fun onClick(view: View) {
+        when (view.id) {
+            R.id.btn_choose -> doSelectNumber()
+        }
+    }
+
+    companion object {
+        const val EXTRA_SELECTED_VALUE = "extra_selected_value"
+        const val RESULT_CODE = 110
+    }
+}
